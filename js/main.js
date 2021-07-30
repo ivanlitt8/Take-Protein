123e3d34c1af5a8258a54381edfe8be22bb15b1c
Iván <ivanlitt8@gmail.com>
2021-07-30 03:05:38 -0300

    catalogo hecho

--- js/main.js
+++ js/main.js
@@ -2,6 +2,7 @@ $(document).ready(function(){
     $('.bxslider').bxSlider({
         mode: 'fade',
         captions: true,
-        slideWidth: 1600
+        slideWidth: 1600,
+        responsive: true
     });
 });
